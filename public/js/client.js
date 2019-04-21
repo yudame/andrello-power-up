9d5311d3cf9a74414caa7d5535f12bbad8780bc1
Glitch (andrello-power-up) <none>
2019-04-21 17:09:03 +0000

    🎸🍫 Updated with Glitch

--- public/js/client.js
+++ public/js/client.js
@@ -5,6 +5,8 @@ window.Promise = TrelloPowerUp.Promise;
 
 var ANDRELLO_ICON = 'https://cdn.glitch.com/3edcc571-a9a4-4249-ae6e-88bff229ff75%2Fjoy.png?1555757158271';
 
+var WHITE_ICON = 'https://cdn.glitch.com/3edcc571-a9a4-4249-ae6e-88bff229ff75%2F9FCB021F-CEDB-43B4-9339-3BF12C5DEAEC.png?1555861308289';
+var BLACK_ICON = 'https://cdn.glitch.com/3edcc571-a9a4-4249-ae6e-88bff229ff75%2F7BA0338A-2DC1-43F0-A464-FBC75F737748.png?1555861308704';
 
 
 
@@ -54,9 +56,44 @@ var getBadge = function(t, detail) {
 TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
   
-  'card-badges': function(t){
-    return getBadge(t, false);
+  'board-buttons': function (t, opts) {
+    var board_id = t.board('id')
+    
+    return [
+      {
+        // we can either provide a button that has a callback function
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON
+        },
+        text: 'Andrello',
+        callback: function(t) {
+            return t.popup({
+              title: "Andrello",
+              url: './board-options.html',
+              height: 200
+            });
+        },
+        condition: 'edit'
+      }, 
+      {
+        // or we can also have a button that is just a simple url
+        // clicking it will open a new tab at the provided url
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON
+        },
+        text: 'View Live Site',
+        condition: 'always',
+        url: 'https://andrello-api-production.herokuapp.com/trello/setup?board_id='+board_id,
+        target: 'Inspiring Boards' // optional target for above url
+      }
+    ];
   },
+  
+  //'card-badges': function(t){
+  //  return getBadge(t, false);
+  //},
   
 	'card-buttons': function(t, options) {
     // check that viewing member has write permissions on this board
@@ -79,9 +116,9 @@ TrelloPowerUp.initialize({
             });
           } else {
             return t.popup({
-              title: "Publish",
-              url: './publish.html',
-              height: 411
+              title: "Andrello",
+              url: './card-options.html',
+              height: 75
             });
           }
         }
